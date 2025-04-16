0394dc158c458b917dc2c46d4ff5e015f94d0729
Sydney Kallus <kallussydney88@gmail.com>
2025-04-16 00:38:30 -0500

    displaying user profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useAuthenticator } from "@aws-amplify/ui-react";
+import {
+  Button,
+  Heading,
+  Flex,
+  View,
+  Grid,
+  Divider,
+} from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
+import "@aws-amplify/ui-react/styles.css";
+import outputs from "../amplify_outputs.json";
 
-function App() {
-  const [count, setCount] = useState(0)
+Amplify.configure(outputs);
+
+export default function App() {
+  const { user, signOut } = useAuthenticator((context) => [context.user]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Flex
+      className="App"
+      justifyContent="center"
+      alignItems="center"
+      direction="column"
+      width="70%"
+      margin="0 auto"
+    >
+      <Heading level={1}>My Profile</Heading>
+      <Divider />
 
-export default App
+
+      <Grid
+        margin="3rem 0"
+        autoFlow="column"
+        justifyContent="center"
+        gap="2rem"
+        alignContent="center"
+      >
+      <Flex
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        gap="2rem"
+        border="1px solid #ccc"
+        padding="2rem"
+        borderRadius="5%"
+        margin="3rem 0"
+      >
+        <View>
+	  <Heading level="3">kallussydney88@gmail.com</Heading>
+          <Heading level="3">Sydney Kallus</Heading>
+        </View>
+      </Flex>
+      </Grid>
+      <Button onClick={signOut}>Sign Out</Button>
+    </Flex>
+  );
+}
